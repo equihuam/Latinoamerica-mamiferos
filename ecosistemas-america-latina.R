9ea978e388a2aacabf61df9ac2efadfef28019f9
Miguel Eq <equihuam@gmail.com>
2025-07-17 01:38:20 -0600

    mapas recortados

--- ecosistemas-america-latina.R
+++ ecosistemas-america-latina.R
@@ -1,7 +1,36 @@
-library(readr)
+pacman::p_load(readr, stringr, dplyr, terra)
 
-ecos_amlat_arch <- list.files("C:/Users/equih/1 Nubes/OneDrive - El Instituto de Ecología/America Latina y el Caribe/", 
-                         pattern = "rle-query-results_rectángulo_america_larina_y_caribe.csv", 
-                         full.names = TRUE)
 
-ecos_amlat <- read_csv(ecos_amlat_arch)
+proyect_lst <- "C:/Users/equih/1 Nubes/OneDrive - El Instituto de Ecología/America Latina y el Caribe/"
+
+ecos_amlat_arch <- list.files(proyect_lst, 
+                              pattern = "rle-query-results_ame.*csv$",
+                              full.names = TRUE)
+
+# Unidades ecológicas en latinoamérica
+ecos_amlat <- read_csv(ecos_amlat_arch, show_col_types = FALSE) |> 
+  select(1:6) |> 
+  unique()
+
+json_lst <- "C:/Users/equih/1 Nubes/OneDrive - El Instituto de Ecología/America Latina y el Caribe/all-maps-vector-geojson"
+
+# Jsons de unidades ecológicas
+ecos_amlat_jsons <- tibble(file = list.files(json_lst)) |>
+  filter(str_detect(file, "No-en|lat-amer", negate = TRUE)) |> 
+  mutate(ID = str_extract(file, "\\w+\\d+\\.\\d+")) |> 
+  relocate(ID, .before = file) |> 
+  right_join(ecos_amlat)
+
+
+map_lim <- vect(paste0(proyect_lst, "/", 
+                       "límites continentales y marinos.shp"))
+for (mapa_n in ecos_amlat_jsons$file[67:69])
+{
+  mapa <- vect(paste0(json_lst, "/", mapa_n))
+  mapa_1 <- crop(mapa, map_lim)
+  writeVector(mapa_1, paste0(json_lst, "/lat-amer/", mapa_n), 
+              filetype = "GeoJSON", overwrite = TRUE)
+}
+
+plot(mapa)
+
